9acd3ecef38b65c94bd8d30f734fc1ad6f8724d3
公司 <951827860@qq.com>
2017-04-26 10:43:11 +0800

    还在排查中的bug,zans暂时跳过了

--- demo/demo01select.js
+++ demo/demo01select.js
@@ -3,7 +3,7 @@
  * http://www.runoob.com/nodejs/nodejs-mongodb.html
  * MangoDB实例: 查询数据
  * 启动服务：  D:\MongoDB\Server\3.4\bin\mongod.exe
- * node .\demo\demo01select
+ * node .\demo\demo01
  * 页面地址：  http://127.0.0.1:8091/demo01list.html
  */
 
@@ -14,12 +14,13 @@ var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://localhost:27017/runoob'; // 数据库为 runoob
 var urlencodedParser = bodyParser.urlencoded({extended: false})//创建 application/x-www-form-urlencoded编码解析
 
-app.use(express.static('public'));
-
 // app.get('/index.htm', function (req, res) {
 //     res.sendFile( __dirname + "/" + "index.htm" );
 // })//屏蔽get方法
 
+/*
+* select查询方法
+* */
 app.post('/note_select', urlencodedParser, function (req, res) {//穿件路由
     select(req, res);
 })
@@ -54,6 +55,55 @@ var select = function (req, res) {
             callback(result);
         });
     }
-
-
 }
+
+/*
+ *   add方法
+ * */
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+app.use(express.static('public'));
+
+// app.get('/index.htm', function (req, res) {
+//     res.sendFile( __dirname + "/" + "index.htm" );
+// })
+
+app.post('/note_add', urlencodedParser, function (req, res) {
+
+    insert(req);
+
+    // 输出 JSON 格式
+    response = {
+        title: req.body.title,
+        content: req.body.content
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+var insert = function (req) {
+
+    var insertData = function (db, callback) {
+        //连接到表 site
+        var collection = db.collection('my_note');
+        //插入数据
+        var data = [{"title": req.body.title, "content": req.body.content, "time": Date.now(), "test": ""}];
+        collection.insert(data, function (err, result) {
+            if (err) {
+                console.log('Error:' + err);
+                return;
+            }
+            callback(result);
+        });
+    }
+
+    MongoClient.connect(DB_CONN_STR, function (err, db) {
+        console.log("连接成功！");
+        insertData(db, function (result) {
+            console.log(result);
+            db.close();
+        });
+    });
+}
+
